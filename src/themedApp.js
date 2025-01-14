aff124141dc7226b7245fddaf88316f7c133ba12
tyler <tylerscott.home@gmail.com>
2025-01-13 19:19:22 -0500

    Updated README.md

--- src/themedApp.js
+++ src/themedApp.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import PlayNotes from "./components/playNotes";
 import Guitar from "./components/guitar/guitar";
